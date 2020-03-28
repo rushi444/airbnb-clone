05ca83f64682ef398e6f9aca9a643f0a50b2276b
rushi444 <rushi44@gmail.com>
2020-03-27 21:16:13 -0700

    type orm now working with env variables

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+type Query {
+    hello(name: String): String!
+}
+`
+
+const resolvers = {
+    Query: {
+        hello: (_: any, { name }: any) => `Hllo ${name || 'World'}`
+    }
+}
+
+console.log("HI RESTART")
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+})
