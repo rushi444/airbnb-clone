0982b9898878a467228ffd303269a058e2a6bcf3
rushi444 <rushi44@gmail.com>
2020-03-29 20:38:00 -0700

    Now able to Register and create users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,42 +1,17 @@
 import "reflect-metadata";
-import { createConnection } from 'typeorm'
-import { User } from './entity/User'
-console.log('ENvironment', process.env)
-createConnection().then(async connection => {
-    
-    console.log('inserting a new user into the database...')
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await connection.manager.save(user)
+import { importSchema } from 'graphql-import'
+import { ApolloServer } from 'apollo-server'
+import { resolvers } from './resolvers'
+import * as path from 'path'
+import { createConnection } from "typeorm";
 
-    console.log("Saved new user with id: " + user.id)
+const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"))
 
-    console.log("loading users from database")
+const server = new ApolloServer({ typeDefs, resolvers })
 
-    const users = await connection.manager.find(User)
-    console.log("loaded users", users)
+createConnection().then(() => {
+    server.listen().then(({ url }) => {
+        console.log(`Server ready at ${url}`)
+    })
+})
 
-    console.log('READY')
-}).catch(error => console.log(error))
-// import { ApolloServer, gql } from 'apollo-server'
-
-// const typeDefs = gql`
-// type Query {
-//     hello(name: String): String!
-// }
-// `
-
-// const resolvers = {
-//     Query: {
-//         hello: (_: any, { name }: any) => `Hello ${name || 'World'}`
-//     }
-// }
-
-// console.log("HI RESTART")
-// const server = new ApolloServer({ typeDefs, resolvers })
-
-// server.listen().then(({ url }) => {
-//     console.log(`Server ready at ${url}`)
-// })
